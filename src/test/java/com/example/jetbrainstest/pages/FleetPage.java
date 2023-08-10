63f5a72ab7bc54735eae8397b1a7a7b63305a390
ne4chelovek <137280143+ne4chelovek@users.noreply.github.com>
2023-08-10 17:46:16 +0300

    Ad "Step"

--- src/test/java/com/example/jetbrainstest/pages/FleetPage.java
+++ src/test/java/com/example/jetbrainstest/pages/FleetPage.java
@@ -1,5 +1,6 @@
 package com.example.jetbrainstest.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -17,6 +18,7 @@ public class FleetPage {
     @FindBy(css = "a[data-test='button' ][href='/fleet/download/']")
     private WebElement downloadButton;
 
+    @Step("Проверка активности кнопки загрузки")
     public Boolean checkIfDownloadButtonIsClickable() {
         LOG.info("Проверка активности кнопки загрузки");
         return downloadButton.isEnabled();
